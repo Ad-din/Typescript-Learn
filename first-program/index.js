8526899fd41c5f6fe411f4cd378ee00e65aa7893
TryingGit <mda126044@gmail.com>
2025-02-11 11:05:53 +0600

    index file added

---
+++ first-program/index.js
@@ -0,0 +1,5 @@
+var num = 434;
+var str = "3343";
+console.log(num, str);
+var a = false;
+console.log(a);
---
+++ first-program/index.ts
@@ -0,0 +1,6 @@
+let num:number= 434;
+const str:string="3343";
+console.log(num,str);
+
+var a:boolean=false;
+console.log(a);
